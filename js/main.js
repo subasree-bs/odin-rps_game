9dc0001afcfee28a8a2eca26e6e8a5630d5c10e5
subasree <subasreebs5@gmail.com>
2022-06-15 10:54:36 +0530

    ADD & UPDATE main.js

---
+++ js/main.js
@@ -0,0 +1,75 @@
+
+// alert("hi");
+
+// document.getElementById("you_r").addEventListener("click",display);
+// function display() {
+//     document.getElementById("you_choice").innerHTML = Date();
+//   }
+//   function reply_click(clicked_id)
+//   {
+//       alert(clicked_id);
+//   }
+
+
+var you_score = document.getElementById("you_score");
+var machine_score = document.getElementById("machine_score");
+var moves_count = document.getElementById("moves_count");
+var you_choice=document.getElementById("you_choice");
+var machine_choice=document.getElementById("machine_choice");
+var moves_result= document.getElementById("moves_result");
+
+
+for(let moves_count=5; moves_count>=0; moves_count--){
+
+
+
+function myFunction(you_choice_val){
+  // var btnval = document.getElementById("you_p");
+  btnval=you_choice_val;
+  // alert(btnval);
+  // document.getElementById("you_choice")=btnval;
+  document.getElementById("you_choice").innerHTML= btnval;
+  
+  var machine_choice_val = Math.floor(Math.random() * 3);
+  
+  switch(machine_choice_val){
+    case 0: machine_choice_val="Rock"; document.getElementById("machine_choice").innerHTML= machine_choice_val;break;
+    case 1: machine_choice_val="Paper"; document.getElementById("machine_choice").innerHTML= machine_choice_val;break;
+    case 2: machine_choice_val="Scissors"; document.getElementById("machine_choice").innerHTML= machine_choice_val;break;
+  }
+
+  if( you_choice_val == machine_choice_val){
+    document.getElementById("moves_result").innerHTML="Same choice... Match DRAW";
+  }
+  else if(you_choice_val=="Rock" && machine_choice_val=="Scissors"){
+    document.getElementById("moves_result").innerHTML="Rock BEATS Scissors ! You get a point!!";
+    // moves_count--;
+    you_score++;
+  }
+  else if(you_choice_val=="Rock" && machine_choice_val=="Paper"){
+    document.getElementById("moves_result").innerHTML="Paper BEATS Rock ! Machine gets a point!";
+    machine_score++;
+  }
+  else if(you_choice_val=="Paper" && machine_choice_val=="Rock"){
+    document.getElementById("moves_result").innerHTML="Paper BEATS Rock ! You get a point!!";
+    you_score++;
+  }
+  else if(you_choice_val=="Paper" && machine_choice_val=="Scissors"){
+    document.getElementById("moves_result").innerHTML="Scissors BEATS Paper ! Machine gets a point!";
+    machine_score++;
+  }
+  else if(you_choice_val=="Scissors" && machine_choice_val=="Paper"){
+    document.getElementById("moves_result").innerHTML="Scissors BEATS Paper ! You get a point!!";
+    you_score++;
+  }
+  else if(you_choice_val=="Scissors" && machine_choice_val=="Rock"){
+    document.getElementById("moves_result").innerHTML="Rock BEATS Scissors ! Machine gets a point!";
+    machine_score++;
+  }
+  else{}
+
+document.getElementById("you_score").innerHTML=you_score;
+document.getElementById("machine_score").innerHTML=machine_score;
+document.getElementById("moves_count").innerHTML=moves_count;
+}
+}
